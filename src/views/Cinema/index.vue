92f3c25af0957e15a9984169cb07be3309dc1635
Your Name <you@example.com>
2019-09-26 15:48:24 +0800

    p6头部和底部完成

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="main">
+        <Header title="我的Cinema" />
+        <div id="content">
+            <Login />
+        </div>
+        <TabBar />
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import TabBar from "@/components/TabBar";
+export default {
+    name : 'Cinema',
+    components : {
+        Header,
+        TabBar,
+    }
+};
+</script>
+
+<style>
---
+++ src/views/Movie/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="main">
+        <Header title="我的Movie" />
+        <div id="content">
+            <Login />
+        </div>
+        <TabBar />
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import TabBar from "@/components/TabBar";
+export default {
+    name : 'Movie',
+    components : {
+        Header,
+        TabBar,
+    }
+};
+</script>
+
+<style>
