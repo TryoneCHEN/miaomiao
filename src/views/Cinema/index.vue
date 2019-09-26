3614743965ae0420f06f60220405ffa3e4bafb4d
Your Name <you@example.com>
2019-09-26 16:48:42 +0800

    p8影院与登录组件完成

--- src/views/Cinema/index.vue
+++ src/views/Cinema/index.vue
@@ -2,7 +2,18 @@
   <div id="main">
         <Header title="我的Cinema" />
         <div id="content">
-            <Login />
+            <div class="cinema_menu">
+				<div class="city_switch">
+					全城 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="brand_swtich">
+					品牌 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="feature_switch">
+					特色 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+			</div>
+            <CiList />
         </div>
         <TabBar />
     </div>
@@ -11,13 +22,19 @@
 <script>
 import Header from "@/components/Header";
 import TabBar from "@/components/TabBar";
+import CiList from '@/components/CiList';
+
 export default {
     name : 'Cinema',
     components : {
         Header,
         TabBar,
+        CiList
     }
 };
 </script>
 
-<style>
+<style scoped>
+#content .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
+</style>
+
--- src/views/Mine/index.vue
+++ src/views/Mine/index.vue
@@ -11,11 +11,13 @@
 <script>
 import Header from "@/components/Header";
 import TabBar from "@/components/TabBar";
+import Login from "@/components/Login";
 export default {
     name : 'Mine',
     components : {
         Header,
         TabBar,
+        Login
     }
 };
 </script>
